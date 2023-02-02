ba7ab0ab72f8cc0022ce34b3f6ca7d27cd1f585d
suxa <suxa5637@mail.ru>
2023-02-02 20:43:24 +0500

    initial commit

---
+++ src/main/java/uz/suxa/converter_onlyoffice/ConverterOnlyofficeApplication.java
@@ -0,0 +1,13 @@
+package uz.suxa.converter_onlyoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterOnlyofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConverterOnlyofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/suxa/converter_onlyoffice/ConverterOnlyofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.suxa.converter_onlyoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterOnlyofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
